8a34ac10cef1f671e07d7b037e9ee0bcc62801af
Haarika Ramadugu <53280706+Haarika701@users.noreply.github.com>
2022-06-11 14:50:29 -0700

    Time: 9694 ms (5.04%), Space: 56.5 MB (8.01%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function(s1, s2) {
+     let k=s1.length;
+    let permuted = s1.split('').sort().join('');
+    let flag = false;
+    for(let i=0;i<s2.length;i++) {
+        if(!s2[i+k-1]) break;
+        if(isPermuted(s2.slice(i,i+k))) {
+            flag = true;
+            break;
+        }
+    }
+    function isPermuted(sub) {
+       return sub.split('').sort().join('') == permuted;
+   }
+    return flag;
+};
